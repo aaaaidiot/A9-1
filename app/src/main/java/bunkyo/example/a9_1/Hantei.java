5178771aadb718a23d2d596ce3b596c1e76a4aea
b9p31100 <b9p31100@shonan.bunkyo.ac.jp>
2021-11-09 09:25:33 +0900

    判定

--- app/src/main/java/bunkyo/example/a9_1/Hantei.java
+++ app/src/main/java/bunkyo/example/a9_1/Hantei.java
@@ -3,6 +3,8 @@ package bunkyo.example.a9_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class Hantei extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class Hantei extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hantei);
+        ((TextView) findViewById(R.id.textView4)).setMovementMethod(new ScrollingMovementMethod());
     }
+
 }
